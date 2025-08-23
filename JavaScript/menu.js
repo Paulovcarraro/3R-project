eb556a2c05a9227b5a1e712396b5c9d4539d3be9
Paulovcarraro <paulovcarraro@gmail.com>
2025-08-23 20:48:32 -0300

    Criação de novo arquivo css e js  para as funcionalidades e estilização do menu das 2 páginas, linkagem dos arquivos e testes ja feito. Remoção da estilização do popup whatsapp do arquivo styles.css.( foi esquecido de ser removido)

---
+++ JavaScript/menu.js
@@ -0,0 +1,48 @@
+// Scrip para Menu Hamburguer apenas em Mobile
+const menuToggle = document.getElementById("menu-toggle");
+const menuIcon = document.getElementById("menu-icon");
+const nav = document.getElementById("nav-menu");
+const overlay = document.getElementById("menu-overlay");
+
+function openMenu() {
+  nav.classList.add("active");
+  menuToggle.classList.add("active");
+  overlay.classList.add("active");
+
+  menuIcon.classList.remove("fa-bars");
+  menuIcon.classList.add("fa-times");
+}
+
+function closeMenu() {
+  nav.classList.remove("active");
+  menuToggle.classList.remove("active");
+  overlay.classList.remove("active");
+
+  menuIcon.classList.remove("fa-times");
+  menuIcon.classList.add("fa-bars");
+}
+
+menuToggle.addEventListener("click", () => {
+  if (nav.classList.contains("active")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+// Fecha ao clicar fora (overlay)
+overlay.addEventListener("click", closeMenu);
+
+// Animação Underline mobile das Seções do Menu Hamburguer
+
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", () => {
+    // Remove .active de todos
+    document
+      .querySelectorAll(".nav a")
+      .forEach((l) => l.classList.remove("active"));
+    // Adiciona .active no link clicado
+    link.classList.add("active");
+    // Não colocamos e.preventDefault() pra não bloquear a navegação
+  });
+});
